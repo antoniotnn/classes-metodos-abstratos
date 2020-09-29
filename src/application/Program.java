1975a3a3f3ee63cab2d90cc36d215ee5e81ae26d
Antonio Pires <tonan@globo.com>
2020-09-29 16:44:50 -0300

    Quase nada de mudança

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,7 +9,17 @@ public class Program {
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 	
+		System.out.print("Enter the number of tax payers: ");
+		int n=sc.nextInt();
+		
+		for (int i=1;i<=n;i++) {
+			System.out.println("Tax payer #"+i+" data: ");
+			System.out.print("Individual or company (i/c)? ");
+			char ch=sc.next().charAt(0);
+			
+		}
 
+		
 		sc.close();
 	}
 
